f42e425e2251357ec6fa6e7ff97a4ad55ea1bd4f
cesargs1989 <cgallegoh@alumnos.unex.es>
2017-03-28 13:07:13 +0200

    agaporni

---
+++ trololo/proyVA/mainwindow.h
@@ -0,0 +1,68 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+#include <rcdraw.h>
+
+
+
+using namespace cv;
+using namespace std;
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer timer;
+
+    VideoCapture *cap;
+    RCDraw *visorS, *visorD;
+    QImage *imgS, *imgD;
+    Mat colorImage, grayImage, destColorImage, destGrayImage;
+    Mat gray2ColorImage, destGray2ColorImage;
+    bool capture, showColorImage, winSelected;
+    Rect imageWindow;
+
+    struct tipo_objeto{
+        vector<Mat> imagenes;
+        std::vector<std::vector<Point2f> > coords;
+        vector<Mat> descriptores;
+    }objeto;
+
+    vector<tipo_objeto> objetos;
+    Ptr<ORB> detector = ORB::create();
+    BFMatcher matcher;
+
+public slots:
+    //void load_from_file();
+    void compute();
+    void start_stop_capture(bool start);
+    void change_color_gray(bool color);
+    void selectWindow(QPointF p, int w, int h);
+    void deselectWindow();
+    void addObject();
+    void removeObject();
+    void match();
+    void showImageObject();
+
+};
+
+
+#endif // MAINWINDOW_H
